bd3fde4cb1a827402454cb292054a0d6a2b66410
Samuel ONeal <sameo@eplus.net>
2017-12-02 14:33:15 -0600

    added params to driver

--- fallout_driver.asm
+++ fallout_driver.asm
@@ -15,12 +15,19 @@ LEN EQU 13
 MAX EQU 20
 
 .DATA
-    passwords   WORD   20 DUP(?)
-    term        BYTE    "x"
+    passwords   WORD   20   DUP(?)
+    term        BYTE        78h
+    size        BYTE        ?
+    count       BYTE        ?
 
 .CODE
     _start:
-        read_passwords passwords, MAX, LEN, term
+        mov al, LEN
+        mov size, al
+        mov al, MAX
+        mov count, al
+
+        read_passwords passwords, count, size, term
 
         INVOKE ExitProcess, 0
     PUBLIC _start
